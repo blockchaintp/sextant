3c58c4965e73f0f6095bd6ea91b806e7267e89cf
Kai Davenport <kaiyadavenport@gmail.com>
2018-09-05 20:49:15 +0100

    cluster creating view

--- src/pages/ClusterView.js
+++ src/pages/ClusterView.js
@@ -8,6 +8,7 @@ import { lighten } from '@material-ui/core/styles/colorManipulator'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 import Loading from '../components/Loading'
 import ClusterDetails from '../components/ClusterDetails'
@@ -27,6 +28,13 @@ const styles = theme => {
     title: {
       marginBottom: theme.spacing.unit * 2,
     },
+    progressContainer: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    progress: {
+      margin: theme.spacing.unit * 2,
+    },
   }
 
 }
@@ -41,6 +49,46 @@ class ClusterView extends React.Component {
     this.props.cluster.loadClusterData()
   }
 
+  getClusterCreating() {
+    const { classes } = this.props
+
+    return (
+      <div className={ classes.progressContainer }>
+        <Typography
+          variant='body2'
+        >
+          Creating
+        </Typography>
+        <CircularProgress
+          className={ classes.progress }
+          size={ 20 }
+        />
+      </div>
+    )
+  }
+
+  getClusterStatus() {
+    const { cluster, classes } = this.props
+    const { currentClusterData } = cluster
+    const { settings, status } = currentClusterData
+
+    if(status.phase == 'creating') {
+      return this.getClusterCreating()
+    }
+    else {
+      return (
+        <div>
+          <Typography
+            variant='subheading'
+          >
+            Unknown Status: { status.phase }
+          </Typography>
+        </div>
+      )
+    }
+    
+  }
+
   render() {
 
     const { cluster, classes } = this.props
@@ -50,7 +98,7 @@ class ClusterView extends React.Component {
         <Loading />
       )
     }
-    
+
     return (
       <Grid
         container
@@ -58,10 +106,30 @@ class ClusterView extends React.Component {
         direction='row'
         className={ classes.container }
       >
+
         <Grid
           item
-          xs={12}
-          sm={4}
+          sm={12}
+          md={8}
+        >
+          <Paper
+            className={ classes.paper }
+          >
+            <Typography
+              variant='title'
+              className={ classes.title }
+            >
+              Cluster Status
+            </Typography>
+
+            { this.getClusterStatus() }
+          </Paper>
+        </Grid>
+
+        <Grid
+          item
+          sm={12}
+          md={4}
         >
           <Paper
             className={ classes.paper }
@@ -78,22 +146,6 @@ class ClusterView extends React.Component {
           </Paper>
         </Grid>
 
-        <Grid
-          item
-          xs={12}
-          sm={8}
-        >
-          <Paper
-            className={ classes.paper }
-          >
-            <Typography
-              variant='title'
-              className={ classes.title }
-            >
-              Cluster Status
-            </Typography>
-          </Paper>
-        </Grid>
       </Grid>
     )
   }
