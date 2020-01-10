cc609d4ecf0d6e0703ce417532a7e4d2a62909d4
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-01-10 14:36:49 -0500

    YAMLEDITOR Remove redux logger
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/store/index.js
+++ src/store/index.js
@@ -4,20 +4,6 @@ import thunk from 'redux-thunk'
 import { router5Middleware } from 'redux-router5'
 import {logger, createLogger} from 'redux-logger'
 
-// This custom logger ignores actions dispatched as a result of loops - predicate filters out actions from the logs based on the function defined
-// remove an include statement to see the associated action type logged in the console (logger must be last in chain of middleware)
-// replace logger with custom logger in middleware in order to view fewer logs 
-const customLogger = createLogger({
-  predicate: (getState, action) => !(
-    action.type.includes("Loading") || 
-    action.type.includes("set") || 
-    action.type.includes("@@router5") || 
-    (action.type.includes('clearError') && action.payload.includes('cluster.list')) ||
-    action.payload.includes("deployment.listResources") ||
-    action.payload.includes("deployment.list")
-  )
-});
-
 import SagaManager from './sagaManager'
 import reducer from './reducer'
 
