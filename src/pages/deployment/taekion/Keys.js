331af1e9b6037ea56e9108313d55b52071a32278
Kai Davenport <kaiyadavenport@gmail.com>
2020-06-10 14:26:25 +0100

    SXT-365: UI skeleton for key table
    Signed-off-by: Kai Davenport <kaiyadavenport@gmail.com>

--- src/pages/deployment/taekion/Keys.js
+++ src/pages/deployment/taekion/Keys.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
+
+import Button from '@material-ui/core/Button'
+import SimpleTable from 'components/table/SimpleTable'
+import SimpleTableDeleteDialog from 'components/table/SimpleTableDeleteDialog'
+import SimpleTableHeader from 'components/table/SimpleTableHeader'
+import SimpleTableActions from 'components/table/SimpleTableActions'
+
+import settings from 'settings'
+
+const AddIcon = settings.icons.add
+const EditIcon = settings.icons.edit
+const DeleteIcon = settings.icons.delete
 
 const styles = theme => ({
   root: {
@@ -8,20 +19,81 @@ const styles = theme => ({
   },
 })
 
+const FIELDS =[{
+  title: 'Name',
+  name: 'name',
+},{
+  title: 'Fingerprint',
+  name: 'fingerprint',
+}]
+
 class TaekionKeys extends React.Component {
 
   render() {
     const {
       classes,
+      keys,
     } = this.props
 
+    const data = keys.map((key, index) => {
+      return {
+        id: key.id,
+        name: key.name,
+        fingerprint: key.fingerprint,
+      }
+    })
+
+    const headerActions = (
+      <div className={ classes.headerActions }>
+        <div className={ classes.addButton }>
+          <Button
+            _ci='addbutton'
+            className={ classes.button }
+            variant= 'contained'
+            color= 'secondary'
+            onClick={() => {}}
+          >
+            Add
+          <AddIcon />
+          </Button>
+        </div>
+      </div>
+    )
+
+    const getActions = () => {
+  
+      const buttons = [{
+        title: 'Edit',
+        icon: EditIcon,
+        handler: (item) => onEdit(item.id),
+      }, {
+        title: 'Delete',
+        icon: DeleteIcon,
+        handler: (item) => onDelete(item.id),
+      }]
+
+      return buttons
+    }
+
     return (
-      <div className={ classes.root }>
-        <Grid container spacing={24}>
-          <Grid item xs={ 12 }>
-            KEYS
-          </Grid>
-        </Grid>
+      <div>
+        <SimpleTableHeader
+          title="Keys"
+          getActions={ () => headerActions }
+        />
+        <SimpleTable
+          pagination
+          data={ data }
+          fields={ FIELDS }
+          getActions={ (item) => {
+            return (
+              <SimpleTableActions
+                item={ item }
+                actions={ getActions(item) }
+              />
+            )
+          }}
+        />
       </div>
     )
   }
