d18de14d3f8f21d12579e32eee94d39649fd5cc9
Kai Davenport <kaiyadavenport@gmail.com>
2020-06-10 15:50:22 +0100

    SXT-365: hook up createKey routes to api
    Signed-off-by: Kai Davenport <kaiyadavenport@gmail.com>

--- src/containers/deployment/taekion/Keys.js
+++ src/containers/deployment/taekion/Keys.js
@@ -8,14 +8,25 @@ import TaekionKeys from 'pages/deployment/taekion/Keys'
 @connect(
   state => {
 
+    const {
+      cluster,
+      id,
+    } = state.router.route.params
+
     return {
+      cluster,
+      deployment: id,
       keys: selectors.taekion.keys(state),
       addKeyWindowOpen: selectors.taekion.addKeyWindowOpen(state),
+      addKeyResult: selectors.taekion.addKeyResult(state),
+      addKeyError: selectors.taekion.errors.createKey(state),
     }
   },
   {
     onOpenAddKeyWindow: () => taekionActions.setAddKeyWindowOpen(true),
     onCloseAddKeyWindow: () => taekionActions.setAddKeyWindowOpen(false),
+    onCloseKeyResultWindow: () => taekionActions.setAddKeyResult(null),
+    onCreateKey:  taekionActions.createKey,
   },
 )
 class TaekionKeysContainer extends React.Component {
