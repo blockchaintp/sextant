25018138dbf78caf025cfb6a98f44a50bd253d61
Kai Davenport <kaiyadavenport@gmail.com>
2020-06-11 16:43:28 +0100

    SXT-362: filter snapshot page by volume
    Signed-off-by: Kai Davenport <kaiyadavenport@gmail.com>

--- src/containers/deployment/taekion/Snapshots.js
+++ src/containers/deployment/taekion/Snapshots.js
@@ -2,29 +2,36 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import taekionActions from 'store/modules/taekion'
+import routerActions from 'store/modules/router'
 import selectors from 'store/selectors'
 
 import TaekionSnapshots from 'pages/deployment/taekion/Snapshots'
 
+const changeVolume = (volume, params) => routerActions.navigateTo('deployment_settings.taekionSnapshots', Object.assign({}, params, {
+  volume,
+}))
+
 @connect(
   state => {
+    const params = state.router.route.params
 
     const {
       cluster,
       id,
       volume,
-    } = state.router.route.params
+    } = params
 
     return {
       cluster,
       deployment: id,
+      params,
       volume,
       volumes: selectors.taekion.volumes(state),
       snapshots: selectors.taekion.snapshots(state),
     }
   },
   {
-    
+    onChangeVolume: changeVolume,
   },
 )
 class TaekionSnapshotsContainer extends React.Component {
