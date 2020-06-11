de7e545bc88155ce50a72767a6d32d1bee2eba24
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-06-11 12:31:19 -0400

    SXT-364 Change error handling function, load volume on state as initial values in form for update
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/pages/deployment/taekion/Volumes.js
+++ src/pages/deployment/taekion/Volumes.js
@@ -128,6 +128,7 @@ class TaekionVolumes extends React.Component {
       volumes,
       addVolumeWindowOpen,
       addVolumeError,
+      updateVolumeError,
       onOpenAddVolumeWindow,
       onCloseAddVolumeWindow,
       onCreateVolume,
@@ -290,7 +291,7 @@ class TaekionVolumes extends React.Component {
           addVolumeWindowOpen && (
             <FormWrapper
               schema={ FORM_SCHEMA }
-              initialValues={ FORM_INITIAL_VALUES }
+              initialValues={ values }
               error={ addVolumeError }
               onSubmit={ onSubmitForm }
               hooks={ hooks }
@@ -350,8 +351,8 @@ class TaekionVolumes extends React.Component {
           editVolume && (
             <FormWrapper
               schema={FORM_SCHEMA}
-              initialValues={FORM_INITIAL_VALUES}
-              error={addVolumeError}
+              initialValues={editVolume}
+              error={updateVolumeError}
               onSubmit={onSubmitEditedForm}
               hooks={hooks}
               renderForm={({
@@ -373,7 +374,7 @@ class TaekionVolumes extends React.Component {
                       <Button onClick={onCancelEditVolume}>
                         Cancel
                       </Button>
-                      <Button onClick={onSubmitEditedForm} variant="contained" color="secondary" autoFocus>
+                      <Button onClick={handleSubmit} variant="contained" color="secondary" autoFocus>
                         Add
                       </Button>
                     </DialogActions>
