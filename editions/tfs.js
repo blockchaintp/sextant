011bddccb737b285ee636037bedb667830a1d70d
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-07-28 08:59:24 -0400

    fix(header): change Taekion to TFS everywhere it renders in the UI
    sxt-395
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

---
+++ editions/tfs.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright © 2020 Blockchain Technology Partners Limited All Rights Reserved
+ *
+ * License: Product
+ */
+
+// Edition object for dev-mode
+
+const edition = {
+  header: {
+    logo: "/large-full-whiteoutline-roundel.svg",
+    text: "TFS",
+  }
+}
+
+export default edition
