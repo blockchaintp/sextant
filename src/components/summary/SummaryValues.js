ae80ced3ddedb8e95480b1ea111a51e440be2081
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-07-30 14:24:25 -0400

    feat(summaryvalues): render container values on lines, display info on hover
    sxt-325
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/components/summary/SummaryValues.js
+++ src/components/summary/SummaryValues.js
@@ -4,57 +4,70 @@ import { withStyles } from '@material-ui/core/styles'
 
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import Tooltip from '@material-ui/core/Tooltip'
 
-const styles = theme => {
-  return {
-    rightAlign: {
-      textAlign: 'right',
-      paddingRight: theme.spacing.unit * 2,
-    }
+const styles = (theme) => ({
+  rightAlign: {
+    textAlign: 'right',
+    paddingRight: theme.spacing.unit * 2,
+  },
+})
+
+const formatSummaryValues = (value) => {
+  if (Array.isArray(value)) {
+    return value.map((item) => (
+      <Tooltip title={item}>
+        <Typography noWrap="true">
+          {item}
+        </Typography>
+      </Tooltip>
+    ))
   }
+  return (
+    <Typography>
+      {value}
+    </Typography>
+  )
 }
 
 class SummaryValues extends React.Component {
-  
-  
   render() {
     const {
       classes,
       data,
     } = this.props
 
-    const parts = data.reduce((all, row, i) => {
-      return all.concat([
-        <Grid
-          key={ `title-${i}` }
-          item
-          xs={6}
-        >
-          <Typography className={ classes.rightAlign }>
-            <strong>{ row.title }:</strong>
-          </Typography>
-        </Grid>,
-        <Grid
-          key={ `value-${i}` }
-          item
-          xs={6}
-        >
-          <Typography>
-            { row.value }
-          </Typography>
-        </Grid>
-      ])
-    }, [])
+    const parts = data.reduce((all, row, i) => all.concat([
+      <Grid
+        key={`title-${i}`}
+        item
+        xs={6}
+      >
+        <Typography className={classes.rightAlign}>
+          <strong>
+            {row.title}
+            :
+          </strong>
+        </Typography>
+      </Grid>,
+      <Grid
+        key={`value-${i}`}
+        item
+        xs={6}
+      >
+        {formatSummaryValues(row.value)}
+      </Grid>,
+    ]), [])
 
     return (
       <Grid
         container
-        direction='row'
-        className={ classes.container }
-        spacing={ 8 }
+        direction="row"
+        className={classes.container}
+        spacing={8}
       >
 
-      { parts }
+        {parts}
 
       </Grid>
     )
