10e471071f704f18c2afd23197dd3072717c8a48
Mikeala Sheldt <mikaela@blockchaintp.com>
2021-03-08 11:06:56 -0500

    feat(role-table): add edit option dialog components and lint existing components
    sxt-235
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/pages/role/RoleTable.js
+++ src/pages/role/RoleTable.js
@@ -1,26 +1,23 @@
+/* eslint-disable no-shadow */
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import Button  from '@material-ui/core/Button'
+import Button from '@material-ui/core/Button'
 
 import SimpleTable from 'components/table/SimpleTable'
 import SimpleTableDeleteDialog from 'components/table/SimpleTableDeleteDialog'
 import SimpleTableHeader from 'components/table/SimpleTableHeader'
 import SimpleTableActions from 'components/table/SimpleTableActions'
 
-import MenuButton from 'components/layout/MenuButton'
-
-import RoleForm from './RoleForm'
-
 import settings from 'settings'
+import RoleForm from './RoleForm'
+import EditRoleDialog from './EditRoleDialog'
 
 const AddIcon = settings.icons.add
 const EditIcon = settings.icons.edit
 const DeleteIcon = settings.icons.delete
-const DeploymentIcon = settings.icons.deployment
-const ViewIcon = settings.icons.view
 
-const styles = theme => ({
+const styles = (theme) => ({
   errorText: {
     color: theme.palette.error.main,
   },
@@ -50,10 +47,11 @@ const styles = theme => ({
 })
 
 class RoleTable extends React.Component {
-
   state = {
     deleteConfirmOpen: false,
     deleteConfirmItem: null,
+    editConfirmOpen: false,
+    editConfirmItem: null,
   }
 
   openDeleteDialog(item) {
@@ -69,14 +67,28 @@ class RoleTable extends React.Component {
     })
   }
 
+  openEditDialog(item) {
+    this.setState({
+      editConfirmOpen: true,
+      editConfirmItem: item,
+    })
+  }
+
+  closeEditDialog() {
+    this.setState({
+      editConfirmOpen: false,
+    })
+  }
+
   render() {
-    const { 
+    const {
       classes,
       title,
       headerClassname,
       roles,
       onAdd,
       onDelete,
+      onEdit,
       onCancel,
 
       open,
@@ -88,15 +100,17 @@ class RoleTable extends React.Component {
       setSearch,
       loadUsers,
       clearUsers,
-      
+
     } = this.props
 
     const {
       deleteConfirmOpen,
       deleteConfirmItem,
+      editConfirmOpen,
+      editConfirmItem,
     } = this.state
 
-    const fields =[{
+    const fields = [{
       title: 'User',
       name: 'user',
     }, {
@@ -104,22 +118,20 @@ class RoleTable extends React.Component {
       name: 'role',
     }]
 
-    const data = roles.map((role, index) => {
-      return {
-        id: role.id,
-        user: role.userRecord.username,
-        userid: role.userRecord.id,
-        role: role.permission,
-      }
-    })
+    const data = roles.map((role) => ({
+      id: role.id,
+      user: role.userRecord.username,
+      userid: role.userRecord.id,
+      role: role.permission,
+    }))
 
     const headerActions = (
-      <div className={ classes.headerActions }>
-        <div className={ classes.addButton }>
-          <Button 
-            onClick={ () => setOpen(true) } 
-            variant="contained" 
-            color="secondary" 
+      <div className={classes.headerActions}>
+        <div className={classes.addButton}>
+          <Button
+            onClick={() => setOpen(true)}
+            variant="contained"
+            color="secondary"
           >
             Add
             <AddIcon />
@@ -128,54 +140,69 @@ class RoleTable extends React.Component {
       </div>
     )
 
+    // eslint-disable-next-line no-unused-vars
     const getActions = (item) => {
-      const baseActions = [{
-        title: 'Delete',
-        icon: DeleteIcon,
-        handler: (item) => this.openDeleteDialog(item),
-      }]
+      const baseActions = [
+        {
+          title: 'Delete',
+          icon: DeleteIcon,
+          handler: (item) => this.openDeleteDialog(item),
+        },
+        {
+          title: 'Edit',
+          icon: EditIcon,
+          handler: (item) => this.openEditDialog(item),
+        },
+      ]
       return baseActions
     }
 
     return (
       <div>
         <SimpleTableHeader
-          title={ title || 'Roles' }
-          getActions={ () => headerActions }
-          className={ headerClassname }
+          title={title || 'Roles'}
+          getActions={() => headerActions}
+          className={headerClassname}
         />
         <SimpleTable
-          data={ data }
-          fields={ fields }
-          getActions={ (item) => {
-            return (
-              <SimpleTableActions
-                item={ item }
-                actions={ getActions(item) }
-              />
-            )
-          }}
+          data={data}
+          fields={fields}
+          getActions={(item) => (
+            <SimpleTableActions
+              item={item}
+              actions={getActions(item)}
+            />
+          )}
         />
         <RoleForm
-          title={ title || 'Role' }
-          open={ open }
-          level={ level }
-          search={ search }
-          users={ users }
-          setSearch={ setSearch }
-          setLevel={ setLevel }
-          loadUsers={ loadUsers }
-          clearUsers={ clearUsers }
-          onCancel={ onCancel }
-          onConfirm={ onAdd }
+          title={title || 'Role'}
+          open={open}
+          level={level}
+          search={search}
+          users={users}
+          setSearch={setSearch}
+          setLevel={setLevel}
+          loadUsers={loadUsers}
+          clearUsers={clearUsers}
+          onCancel={onCancel}
+          onConfirm={onAdd}
         />
         <SimpleTableDeleteDialog
-          open={ deleteConfirmOpen }
-          title={ deleteConfirmItem ? `the role for ${deleteConfirmItem.user}` : null }
-          onCancel={ () => this.closeDeleteDialog() }
-          onConfirm={ () => {
+          open={deleteConfirmOpen}
+          title={deleteConfirmItem ? `the role for ${deleteConfirmItem.user}` : null}
+          onCancel={() => this.closeDeleteDialog()}
+          onConfirm={() => {
             this.closeDeleteDialog()
             onDelete(deleteConfirmItem.userid)
+          }}
+        />
+        <EditRoleDialog
+          item={editConfirmItem}
+          open={editConfirmOpen}
+          onCancel={() => this.closeEditDialog()}
+          onConfirm={(editeditem) => {
+            this.closeEditDialog()
+            onEdit(editeditem)
           }}
         />
       </div>
@@ -191,4 +218,3 @@ RoleTable.propTypes = {
 }
 
 export default withStyles(styles)(RoleTable)
-
