27fd56c47db957a2020331936e7a4fc396f9d03f
Mikeala Sheldt <mikaela@blockchaintp.com>
2021-05-04 09:31:11 -0400

    feat(delete-dialog): add text field to delete resource dialog
    sxt-399
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

--- src/components/table/DeletePodDialog.js
+++ src/components/table/DeletePodDialog.js
@@ -7,15 +7,29 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 const styles = () => ({
 })
 
 class DeletePodDialog extends React.Component {
+  state = {
+    name: '',
+    error: true,
+  };
+
+  handleChange = (resourceName) => (event) => {
+    const { value } = event.target
+    if (value === resourceName) this.setState({ error: false })
+    else this.setState({ error: true })
+    this.setState({ name: value });
+  }
+
   render() {
     const {
       open, onCancel, onConfirm, title,
     } = this.props
+    const { name, error } = this.state
 
     return (
       <Dialog
@@ -29,12 +43,21 @@ class DeletePodDialog extends React.Component {
           <DialogContentText id="alert-dialog-description">
             Deleting this pod will trigger another to be built in its place.
           </DialogContentText>
+          <TextField
+            helperText="Type the name of the resource you want to delete."
+            error={error}
+            id="standard-name"
+            label="Pod Name"
+            value={name}
+            onChange={this.handleChange(title)}
+            margin="normal"
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={onCancel}>
             Cancel
           </Button>
-          <Button _ci="confirm" onClick={onConfirm} variant="contained" color="secondary" autoFocus>
+          <Button _ci="confirm" disabled={error} onClick={onConfirm} variant="contained" color="secondary" autoFocus>
             Confirm
           </Button>
         </DialogActions>
--- src/components/table/SimpleTableDeleteDialog.js
+++ src/components/table/SimpleTableDeleteDialog.js
@@ -7,6 +7,7 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 import { getDialogDeleteText } from '../../utils/translators'
 
@@ -14,12 +15,25 @@ const styles = () => ({
 })
 
 class SimpleTableDeleteDialog extends React.Component {
+  state = {
+    name: '',
+    error: true,
+  };
+
+  handleChange = (resourceName) => (event) => {
+    const { value } = event.target
+    if (value === resourceName) this.setState({ error: false })
+    else this.setState({ error: true })
+    this.setState({ name: value });
+  }
+
   render() {
     const {
-      open, onCancel, onConfirm, title, resource,
+      open, onCancel, onConfirm, title, resource, resourceType,
     } = this.props
-
+    const { name, error } = this.state
     const text = resource ? getDialogDeleteText(resource.status) : ''
+    const resourceName = resource ? resource.name : null
 
     return (
       <Dialog
@@ -33,12 +47,21 @@ class SimpleTableDeleteDialog extends React.Component {
           <DialogContentText id="alert-dialog-description">
             {`Are you sure you want to ${text.subtext} ${title}?`}
           </DialogContentText>
+          <TextField
+            helperText="Type the name of the resource you want to delete."
+            error={error}
+            id="standard-name"
+            label={`${resourceType} name`}
+            value={name}
+            onChange={this.handleChange(resourceName)}
+            margin="normal"
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={onCancel}>
             Cancel
           </Button>
-          <Button _ci="confirm" onClick={onConfirm} variant="contained" color="secondary" autoFocus>
+          <Button disabled={error} _ci="confirm" onClick={onConfirm} variant="contained" color="secondary" autoFocus>
             Confirm
           </Button>
         </DialogActions>
