00291624603cdcc680b67586e4fd7081edc24292
Jimmy Holway <jimmy@blockchaintp.com>
2021-09-01 11:26:46 -0700

    build(packages): adds jest-tap-reporter to dev dependencies
    instructs jest-tap-reporter to output tap friendly test results and adds a posttest to remove the file when testing is complete
    
    sxt-759
    
    Signed-off-by: Jimmy Holway <jimmy@blockchaintp.com>

--- src/store/utils/createAction.test.js
+++ src/store/utils/createAction.test.js
@@ -1,18 +1,17 @@
-import tape from 'tape'
+
 import createAction from './createAction'
 
-tape('createAction -> standard', (t) => {
+test('createAction -> standard', () => {
   const actionFactory = createAction('toggleOpen')
   const action = actionFactory(true)
-  t.deepEqual(action, {
+
+  expect(action).toMatchObject({
     type: 'toggleOpen',
     payload: true,
-  }, `the action is correct`)
-  t.end()
+  })
 })
 
-tape('createAction -> action has type field', (t) => {
+test('createAction -> action has type field', () => {
   const actionFactory = createAction('toggleOpen')
-  t.equal(actionFactory.type, 'toggleOpen', `the type field is set on the action`)
-  t.end()
+  expect(actionFactory.type).toBe('toggleOpen')
 })
