e8c75df369f75c97a69d80f36f0e4d64ee04f0df
Jimmy Holway <jimmy@blockchaintp.com>
2023-04-12 17:58:26 -0700

    fix(editions): adds default edition and type, converts file to typescript
    SXT-988
    
    Signed-off-by: Jimmy Holway <jimmy@blockchaintp.com>

---
+++ src/edition.ts
@@ -0,0 +1,17 @@
+// this is a noop file
+
+export type Edition = {
+    header: {
+        logo: string
+        text: string
+    }
+}
+
+const edition: Edition = {
+    header: {
+      logo: 'large-full-whiteoutline-roundel.svg',
+      text: 'Engineering',
+    },
+  }
+
+  export default edition
