1f1804ced31fab2b299e897f8f72c90fa6830fc8
Jimmy Holway <jimmy@blockchaintp.com>
2023-05-03 12:13:18 -0700

    fix(status-taskstatusicon): removes unused styles and more explicitly defines types
    SXT-1011
    
    Signed-off-by: Jimmy Holway <jimmy@blockchaintp.com>

--- src/components/status/TaskStatusIcon.tsx
+++ src/components/status/TaskStatusIcon.tsx
@@ -1,44 +1,23 @@
 import * as React from 'react'
 import Chip from '@mui/material/Chip'
 import Tooltip from '@mui/material/Tooltip'
-import CircularProgressIcon from '@mui/material/CircularProgress'
+import CircularProgress from '@mui/material/CircularProgress'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 import ErrorIcon from '@mui/icons-material/Error'
-import { Theme } from '@mui/material/styles'
-import { makeStyles } from '@mui/styles'
-import { green } from '@mui/material/colors'
 
 interface TaskStatusIconProps {
-  error: string;
+  error: string | null;
   status: 'created' | 'running' | 'finished' | 'error';
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  success: {
-    color: green[600],
-  },
-  error: {
-    color: theme.palette.error.dark,
-  },
-  chip: {
-    height: '38px',
-    borderRadius: '36px',
-  },
-  progress: {
-    marginLeft: '5px',
-  },
-}))
-
 const TaskStatusIcon: React.FC<TaskStatusIconProps> = ({
-  error,
+  error = null,
   status,
 }) => {
-  const classes = useStyles()
   if (status === 'running' || status === 'created') {
     return (
       <Chip
-        className={classes.chip}
-        icon={<CircularProgressIcon size={18} />}
+        icon={<CircularProgress size={18} />}
         sx={{ paddingLeft: '6px' }}
         label={status}
       />
@@ -48,7 +27,6 @@ const TaskStatusIcon: React.FC<TaskStatusIconProps> = ({
     return (
       <Tooltip title={error}>
         <Chip
-          className={classes.chip}
           icon={<ErrorIcon />}
           label="error"
           color="error"
@@ -59,7 +37,6 @@ const TaskStatusIcon: React.FC<TaskStatusIconProps> = ({
   if (status === 'finished') {
     return (
       <Chip
-        className={classes.chip}
         icon={<CheckCircleIcon />}
         label={status}
       />
