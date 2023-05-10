0c3f1fb5d7e40a0b5b0df3b96409eb2d44b4124f
Jimmy Holway <jimmy@blockchaintp.com>
2023-05-10 14:22:40 -0700

    refactor(servicetable): refactors ports logic to not use nested template literals, removes unused @mui/styles
    SXT-1012
    
    Signed-off-by: Jimmy Holway <jimmy@blockchaintp.com>

--- src/components/deployment/ServiceTable.js
+++ src/components/deployment/ServiceTable.js
@@ -1,10 +1,7 @@
 /* eslint-disable max-len */
 import React from 'react'
-import withStyles from '@mui/styles/withStyles';
 
 import SimpleTable from 'components/table/SimpleTable'
-
-const styles = () => ({})
 
 const fields = [{
   title: 'Name',
@@ -40,7 +37,16 @@ class ServiceTable extends React.Component {
           name: service.metadata.name,
           created: service.metadata.creationTimestamp ? new Date(service.metadata.creationTimestamp).toLocaleString() : '',
           type: service.spec.type ? service.spec.type : '',
-          ports: (service.spec.ports && service.spec.ports.length > 0) ? service.spec.ports.map((port) => `${port.port === port.targetPort ? port.port : `${port.port}:${port.targetPort}`}/${port.protocol}`).join(', ') : '',
+          ports: (service.spec.ports && service.spec.ports.length > 0)
+            ? service.spec.ports
+              .map((port) => {
+                const portDisplay = port.port === port.targetPort
+                  ? port.port
+                  : `${port.port}:${port.targetPort}`;
+                return `${portDisplay}/${port.protocol}`;
+              })
+              .join(', ')
+            : '',
           clusterIp: service.spec.clusterIP ? service.spec.clusterIP : '',
           externalIp,
         }
@@ -55,4 +61,4 @@ class ServiceTable extends React.Component {
   }
 }
 
-export default withStyles(styles)(ServiceTable)
+export default ServiceTable
