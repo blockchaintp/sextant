8f06c53a10f19d69cc3557965ada65215a5afa02
Jimmy Holway <jimmy@blockchaintp.com>
2023-05-11 15:30:05 -0700

    refactor(select): refactors to typescript functional component, @mui/styles -> @mui/system
    SXT-1012
    
    Signed-off-by: Jimmy Holway <jimmy@blockchaintp.com>

--- src/components/form/fields/Select.tsx
+++ src/components/form/fields/Select.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import withStyles from '@mui/styles/withStyles';
+import * as React from 'react'
+import { styled } from '@mui/system';
 import Select from '@mui/material/Select'
 import FormControl from '@mui/material/FormControl'
 import MenuItem from '@mui/material/MenuItem'
@@ -7,31 +7,70 @@ import FormLabel from '@mui/material/FormLabel';
 
 import HelperText from './HelperText'
 
-const styles = (theme) => ({
-  root: {
-    display: 'flex',
-    marginTop: theme.spacing(2),
-  },
-})
+const StyledFormControl = styled(FormControl)(({ theme }) => ({
+  display: 'flex',
+  marginTop: theme.spacing(2),
+}))
 
-class SelectField extends React.Component {
-  render() {
-    const {
-      field: {
-        name,
-        value,
-        onChange,
-      },
-      error,
-      touched,
-      item,
-      classes,
-      disabled,
-      formProps,
-    } = this.props
+interface Option {
+  title: string
+  value: string
+  blurb?: string
+}
+
+interface Item {
+  alternateText: string
+  helperText: string
+  title: string
+  options: Option[]
+  extraProps?: object
+}
+
+interface SelectFieldProps {
+  field: {
+    name: string
+    value: string
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  }
+  error: string
+  touched: boolean
+  item: Item
+  disabled: boolean
+  formProps: {
+    touched: boolean
+    values: {
+      [key: string]: {
+        [key: string]: string
+      }
+    }
+    errors: {
+      [key: string]: string
+    }
+    hooks: {
+      disabled: boolean
+      validate: boolean
+      hidden: {
+        [key: string]: string | boolean
+      }
+    }
+  }
+}
+
+const SelectField: React.FC<SelectFieldProps> = ({
+  field: {
+    name,
+    value,
+    onChange,
+  },
+  error,
+  touched,
+  item,
+  disabled,
+  formProps,
+}) => {
 
     // filter through the consensus options, find the selected consensus, return the corresponding blurb
-    const blurbText = (selectedConsensus) => {
+    const blurbText = (selectedConsensus: string) => {
       for (const option of item.options) {
         if (option.value === selectedConsensus) {
           return option.blurb
@@ -44,9 +83,10 @@ class SelectField extends React.Component {
     const extraProps = item.extraProps || {}
 
     return (
-      <FormControl component="fieldset" className={classes.root} error={!!(touched && error)}>
+      <StyledFormControl error={!!(touched && error)}>
         <FormLabel htmlFor={name}>{ title }</FormLabel>
         <Select
+          // @ts-ignore
           _ci={name}
           id={`select_${name}`}
           value={value || ''}
@@ -82,9 +122,8 @@ class SelectField extends React.Component {
           touched={touched}
         />
 
-      </FormControl>
+      </StyledFormControl>
     )
-  }
 }
 
-export default withStyles(styles)(SelectField)
+export default SelectField
