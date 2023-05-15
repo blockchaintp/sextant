bc7e990a49cee779e07569a56cf34fbcec437929
Jimmy Holway <jimmy@blockchaintp.com>
2023-05-15 09:12:30 -0700

    refactor(clusterstatus): @mui/styles -> @mui/system
    SXT-1012
    
    Signed-off-by: Jimmy Holway <jimmy@blockchaintp.com>

--- src/pages/cluster/ClusterStatus.js
+++ src/pages/cluster/ClusterStatus.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import withStyles from '@mui/styles/withStyles';
+import { styled } from '@mui/system';
 import Grid from '@mui/material/Grid'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography'
@@ -10,73 +9,63 @@ import TaskTable from 'components/task/TaskTable'
 import NodeTable from 'components/cluster/NodeTable'
 import SummaryValues from 'components/summary/SummaryValues'
 
-const styles = (theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    margin: theme.spacing(2),
-  },
-})
+const Root = styled('div')(({ theme }) => ({
+  padding: theme.spacing(2),
+}))
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  margin: theme.spacing(2),
+}))
 
 class ClusterStatus extends React.Component {
   render() {
     const {
-      classes,
       resources,
       summary,
       tasks,
     } = this.props
 
     return (
-      <div className={classes.root}>
+      <Root>
         <Grid container spacing={3}>
           <Grid item xs={9}>
-            <Paper className={classes.paper}>
+            <StyledPaper>
               <Typography variant="h6" gutterBottom>
                 Nodes
               </Typography>
               <NodeTable
                 data={resources.nodes}
               />
-            </Paper>
-            <Paper className={classes.paper}>
+            </StyledPaper>
+            <StyledPaper>
               <DeploymentTable
                 embedded
               />
-            </Paper>
-            <Paper className={classes.paper}>
+            </StyledPaper>
+            <StyledPaper>
               <Typography variant="h6" gutterBottom>
                 Tasks
               </Typography>
               <TaskTable
                 data={tasks}
               />
-            </Paper>
+            </StyledPaper>
           </Grid>
           <Grid item xs={3}>
-            <Paper className={classes.paper}>
+            <StyledPaper>
               <Typography variant="h6" gutterBottom>
                 Summary
               </Typography>
               <SummaryValues
                 data={summary}
               />
-            </Paper>
+            </StyledPaper>
           </Grid>
         </Grid>
-      </div>
+      </Root>
     )
   }
 }
 
-ClusterStatus.propTypes = {
-  classes: PropTypes.object.isRequired,
-}
-
-ClusterStatus.defaultProps = {
-
-}
-
-export default withStyles(styles)(ClusterStatus)
+export default ClusterStatus
