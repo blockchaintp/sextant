ece7871e56aa4f79725516afde99ff80621675d0
Jimmy Holway <jimmy@blockchaintp.com>
2023-07-25 18:59:24 +0200

    feat(router): creates route for chronicle explorer starting point
    SXT-1023
    
    Signed-off-by: Jimmy Holway <jimmy@blockchaintp.com>

--- src/router/Router.js
+++ src/router/Router.js
@@ -33,6 +33,8 @@ import DeploymentSettingsDamlParties from 'containers/deployment/daml/Deployment
 import DeploymentSettingsDamlArchives from 'containers/deployment/daml/DeploymentSettingsDamlArchives'
 import DeploymentSettingsDamlTimeService from 'containers/deployment/daml/DeploymentSettingsDamlTimeService'
 
+import DeploymentChronicle from 'containers/deployment/chronicle/Chronicle'
+
 import DeploymentSettingsTaekionCli from 'containers/deployment/taekion/Cli'
 import DeploymentSettingsTaekionKeys from 'containers/deployment/taekion/Keys'
 import DeploymentSettingsTaekionVolumes from 'containers/deployment/taekion/Volumes'
